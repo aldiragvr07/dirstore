c206a9cbe5f17b8c52030e71f9e727fa6bbfffc8
Aldira Givari <aldiragvr07@gmail.com>
2025-07-09 20:59:44 +0700

    sorting,search,filters

--- app/Data/ProductCollectionData.php
+++ app/Data/ProductCollectionData.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Data;
 
+
+use App\Models\Tag;
 use Spatie\LaravelData\Data;
 
 class ProductCollectionData extends Data
@@ -14,13 +16,13 @@ class ProductCollectionData extends Data
         public int $product_count,
 
     ) {}
-    // public static function fromModel(Tag $tag) : self
-    // {
-    //     // return new self(
-    //     //     $tag->id,
-    //     //     $tag->name,
-    //     //     $tag->slug,
-    //     //     $tag->products_count,
-    //     // );
-    // }
+    public static function fromModel(Tag $tag) : self
+    {
+        return new self(
+            $tag->id,
+            (String) $tag->name,
+            (String) $tag->slug,
+            $tag->products_count,
+        );
+    }
 }
