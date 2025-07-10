6f90ab046e41b15119b3ae13142c74ff45105db8
Aldira Givari <aldiragvr07@gmail.com>
2025-07-10 21:54:26 +0700

    cart system

---
+++ app/Contract/CartServiceInterface.php
@@ -0,0 +1,14 @@
+<?php 
+declare(strict_types=1);
+
+namespace App\Contract;
+use App\Data\CartItemData;
+use App\Data\CartData;
+
+interface CartServiceInterface
+{
+    public function addOrUpdate(CartItemData $item) : void;
+    public function remove(string $sku) : void;
+    public function getItemBySku(string $sku) : ?CartItemData;
+    public function all() : CartData;
+}
